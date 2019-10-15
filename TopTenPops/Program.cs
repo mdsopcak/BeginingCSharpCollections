e7ff4591a1b28f16ee568d921bf62b6b6da1fd01
Mark Sopcak <mdsopcak@gmail.com>
2019-10-15 16:55:46 -0400

    Module-3-start

--- TopTenPops/Program.cs
+++ TopTenPops/Program.cs
@@ -10,6 +10,15 @@ namespace TopTenPops
     {
         static void Main(string[] args)
         {
+            string filePath = @"";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
         }
     }
 }
