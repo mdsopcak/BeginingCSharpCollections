c666798de93edd8627e1945e2d022a1f78037304
Mark Sopcak <mdsopcak@gmail.com>
2019-12-13 10:11:13 -0500

    Finished Module 5

--- TopTenPops/CsvReader.cs
+++ TopTenPops/CsvReader.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TopTenPops
+namespace ReadAllCountries
 {
     class CsvReader
     {
@@ -16,19 +16,20 @@ namespace TopTenPops
             this._csvFilePath = csvFilePath;
         }
 
-        public Country[] ReadFirstNCountries(int nCountries)
+        public Dictionary<string, Country> ReadAllCountries()
         {
-            Country[] countries = new Country[nCountries];
+            var countries = new Dictionary<string, Country>();
 
             using (StreamReader sr = new StreamReader(_csvFilePath))
             {
                 //read header line
                 sr.ReadLine();
 
-                for (int i = 0; i < nCountries; i++)
+                string csvLine;
+                while ((csvLine = sr.ReadLine()) != null)
                 {
-                    string csvLine = sr.ReadLine();
-                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                    Country country = ReadCountryFromCsvLine(csvLine);
+                    countries.Add (country.Code, country);
                 }
             }
 
@@ -37,13 +38,32 @@ namespace TopTenPops
 
         public Country ReadCountryFromCsvLine(string csvLine)
         {
-            string[] parts = csvLine.Split(new char[] { ',' });
+            string[] parts = csvLine.Split(',');
+            string name;
+            string code;
+            string region;
+            string popText;
+            switch (parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
+            }
 
-            string name = parts[0];
-            string code = parts[1];
-            string region = parts[2];
-            int population = int.Parse(parts[3]);
-
+            // TryParse leaves population=0 if can't parse
+            int.TryParse(popText, out int population);
             return new Country(name, code, region, population);
         }
     }
--- TopTenPops/Program.cs
+++ TopTenPops/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TopTenPops
+namespace ReadAllCountries
 {
     class Program
     {
@@ -12,13 +12,17 @@ namespace TopTenPops
         {
             string filePath = @"C:\Users\marks\source\repos\BeginingCSharpCollections\TopTenPops\Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
 
-            Country[] countries = reader.ReadFirstNCountries(10);
+            Console.WriteLine("Which country code do you want to look up? ");
+            string userInput = Console.ReadLine();
 
-            foreach (Country country in countries)
-            {
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation (country.Population).PadLeft(15)}: {country.Name}");
-            }
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if (!gotCountry)
+                Console.WriteLine($"Sorry, there is no country with code, {userInput}");
+            else
+                Console.WriteLine($"{country.Name} has population {PopulationFormatter.FormatPopulation(country.Population)}");
+
             Console.ReadLine();
         }
     }
